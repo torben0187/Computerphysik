ab0d86a098cdb63dc6d97bd8399b7371c041ba91
Torben Bredenbach <bredenbach.torben@icloud.com>
2026-06-09 08:58:07 +0200

    üb

--- Code.jl
+++ Code.jl
@@ -1,3 +1,18 @@
-for k in 5:-1:1
-    println(k)
+using LinearAlgebra
+
+
+
+
+
+
+
+
+function pseudo_inverse_with_trunc(A,tol=1e-15)
+
+    U, sigma, V = svd(A)
+    idx = findfirst(x -> x <= 1e-15, sigma)
+    sigma[idx:end] .= 0
+    S = Diagonal(simga)
+    pseudo_inv = V * inv(S) * U'
+    return pseudo_inv
 end
