4235881b8609b67a124c08dd9c49fd51feb0e617
Torben Bredenbach <bredenbach.torben@icloud.com>
2026-06-22 11:46:36 +0200

    1

--- Code.jl
+++ Code.jl
@@ -1,18 +1,2 @@
 using LinearAlgebra
-
-
-
-
-
-
-
-
-function pseudo_inverse_with_trunc(A,tol=1e-15)
-
-    U, sigma, V = svd(A)
-    idx = findfirst(x -> x <= 1e-15, sigma)
-    sigma[idx:end] .= 0
-    S = Diagonal(simga)
-    pseudo_inv = V * inv(S) * U'
-    return pseudo_inv
-end
+u_next = ones(4)
